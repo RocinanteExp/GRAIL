8672762265de9f47bbf10f54a61e0dfa16aec863
Francesco Xia <s276509@studenti.polito.it>
2020-07-06 17:12:47 +0200

    feat: add multi thread solving queries and printing to file

--- src/main.c
+++ src/main.c
@@ -38,28 +38,32 @@ void malloc_performance(){
 }
 
 void* start_thread(void* arg) {
-    char buffer[200000];
-
-    for(int i = 0; i < 10; i++) { 
-        fgets(buffer, 200000, fin); 
-        printf("line %s\n", buffer);
+    char buffer[30];
+    static uint32_t iter = 0;
+    while(fgets(buffer, 30, fin)) { 
+        if(iter < 30) {
+            iter++;
+            printf("line %s\n", buffer);
+        }
     }
 
+    printf("line %s\n", buffer);
+
     return NULL;
-    
-}
+};
 
 void test() {
 
-    const int MAX_THREADS = 4;
-    fin = fopen("./v500000e1000.gra", "r");
+    const int MAX_THREADS = 1;
+    fin = fopen("test/input/v500000e1000.que", "r");
     if(fin == NULL){
-        fprintf(stdout, "ERROR opening file\n");
+        fprintf(stdout, "FAILED opening file at test\n");
         return;
     }
 
     pthread_t thread_ids[MAX_THREADS];
 
+    clock_t start = clock();
     for(int i = 0; i < MAX_THREADS; i++) {
         int err = pthread_create(&thread_ids[i], NULL, start_thread, NULL); 
         if(err != 0) {
@@ -72,21 +76,31 @@ void test() {
         pthread_join(thread_ids[i], NULL);
     }
 
+    clock_t end = clock();
+    printf("DIFFERENCE %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+
 }
 
 int main(int argc, char **argv) {
 
-    Graph* graph = graph_create("test/input/grafo20.gra", 5);
+#if 0
+    Graph* graph = graph_create("test/input/v500000e1000.gra", 2);
+    graph_randomize_labelling_sequential(graph, 2);
+    query_init("test/input/v500000e1000.que", graph);
+#else
+    Graph* graph = graph_create("test/input/grafo20.gra", 2);
+    graph_randomize_labelling_sequential(graph, 2);
     graph_print(graph, true, -1); 
-    graph_randomize_labelling_sequential(graph, graph->num_intervals);
-    graph_print(graph, true, -1); 
-    //Graph* graph = graph_create("./test/input/v500000e1000.gra", 5);
-    //graph_print(graph, true, 203); 
-    //graph_print(graph, true, 8888); 
-    //graph_print(graph, true, 123456); 
-    //graph_print(graph, true, 399999); 
-    //graph_print(graph, true, 400000); 
-    //graph_print(graph, true, 499999); 
+    query_init("test/input/grafo20_25.que", graph);
+#endif
+
+#if 0
+    clock_t start = clock();
+    graph_print_to_file("test/input/out.gra", graph); 
+    clock_t end = clock();
+    fprintf(stdout, "FINISCHED WRITING FILE\n");
+    printf("It took %fs\n", (double)(end - start) / CLOCKS_PER_SEC);
+#endif
 
     return 0;
 
